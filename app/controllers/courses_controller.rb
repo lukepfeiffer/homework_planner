962147c8eb172862a061a1be73b89f0bcd55cc44
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2015-12-25 10:08:48 -0500

    User creats course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,27 @@ class CoursesController < ApplicationController
     ('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.').truncate(350)
   end
 
+  expose :course
   expose :courses do
     current_user.courses
+  end
+
+  def create
+    course = Course.new(courses_params)
+    course.save
+    redirect_to courses_path
+  end
+
+  private
+  def courses_params
+    params.require(:course).permit(
+      :name,
+      :teacher_name,
+      :user_id,
+      :start_time,
+      :end_time,
+      :days_of_the_week,
+      :description
+    )
   end
 end
